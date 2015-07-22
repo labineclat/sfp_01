d7257f91b4b2de0f422bce9b154fd6311967c4ee
djangoineclat <django.ineclat@gmail.com>
2015-07-22 23:46:58 +0530

    Changed the html for site.

--- sfp/urls.py
+++ sfp/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from itemlist import urls as itemlist_urls
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'',include(itemlist_urls)),
 ]
