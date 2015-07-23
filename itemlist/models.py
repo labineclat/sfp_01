8e8fd44fa5a905a37099852d5a632a7bc4283cd7
djangoineclat <django.ineclat@gmail.com>
2015-07-23 14:43:16 +0530

    bootstrap added2

--- itemlist/models.py
+++ itemlist/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-from django.utils import timezone
+
+
+def content_file_name(instance, filename):
+    return '/'.join(['content', instance.user.username, filename])
+
 
 class Item(models.Model):
 	itemname = models.CharField(max_length=200)
 	discribe = models.TextField()
 	price = models.IntegerField()
-	image = models.ImageField()
+	image = models.ImageField(upload_to=content_file_name)
 	weight = models.IntegerField()
 
 	def __str__(self):
